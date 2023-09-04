056ed44e248b703009d17386be454defeec86e00
BlackOtto9911 <danilova.sofia@gmail.com>
2023-09-04 22:36:30 +0300

    10:36

--- src/myComponents/displays/Display.js
+++ src/myComponents/displays/Display.js
@@ -1,5 +1,4 @@
-import { Route, Routes, HashRouter as BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import { Route, Routes, HashRouter as Router } from 'react-router-dom';
 
 import './Display.css';
 import GreetingDisplay from './GreetingDisplay/GreetingDisplay';
@@ -8,14 +7,14 @@ import SubjectDisplay from './SubjectDisplay/SubjectDisplay';
 function Display() {
   return (
     <div className="display">
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path='/' element={<GreetingDisplay MainColor='#00CECB'></GreetingDisplay>}></Route>
           <Route path='/subject/based-math' element={<SubjectDisplay SubjectName='Базовая математика' MainColor='#00CECB'></SubjectDisplay>}></Route>
           <Route path='/subject/advanced-math' element={<SubjectDisplay SubjectName='Профильная математика' MainColor='#00A09E'></SubjectDisplay>}></Route>
           <Route path='/subject/physics' element={<SubjectDisplay SubjectName='Физика' MainColor='#FFDA15'></SubjectDisplay>}></Route>
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
